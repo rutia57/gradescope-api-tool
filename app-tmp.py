0d4b5f7396e818084d6b56ff0a5e5a501aa214e0
Ruth Rosenblum <ruthros@brandeis.edu>
2026-07-01 11:07:30 -0700

    Revert "fix login"
    This reverts commit e0ae261c32ce4727278e22bcdacbc9230d22ba8c.

---
+++ app-tmp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+pg = st.navigation([
+    st.Page("home.py", title="Gradescope API Tool", default=True),
+    # st.Page(
+    #     "pages/Privacy_Policy.py",
+    #     title="Privacy Policy",
+    #     url_path="privacy_policy",
+    # ),
+])
+
+pg.run()
--- gradescope_auth.py
+++ gradescope_auth.py
@@ -4,7 +4,6 @@ import tempfile
 import shutil
 import os
 import json
-import json5
 import html
 import re
 from bs4 import BeautifulSoup
@@ -83,12 +82,12 @@ def login_with_cookies(cookies):
     resp = session.get(BASE_URL)
     soup = BeautifulSoup(resp.text, "html.parser")
     scripts = soup.find_all("script")
-    user = {}
+    user = None
     for script in scripts:
         if script.string and "bugsnagClient.user" in script.string:
             match = re.search(r"bugsnagClient\.user\s*=\s*({.*?});", script.string)
             if match:
-                user = json5.loads(match.group(1))
+                user = match.group(1)
                 break
     return GSConnectionFromSession(session, user), user
 
